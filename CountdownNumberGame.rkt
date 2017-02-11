bf74903a8fa75c6724b3255138e0ec9de1798ec1
GARY MC HUGH <G00308668@gmit.ie>
2017-02-11 18:55:07 +0000

    combined list and added function to randomly select a number from the a list

--- CountdownNumberGame.rkt
+++ CountdownNumberGame.rkt
@@ -1,10 +1,7 @@
 #lang racket
 
-;a list of the possible small values
-(define small (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10))
-
-;a list of the possible large values
-(define large (list 25 50 75 100))
+;a list containing all possible values
+(define values (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 
 ;generate a target value
 ;https://www.reddit.com/r/Racket/comments/32g9v3/how_do_you_generate_random_numbers/
@@ -12,3 +9,14 @@
 
 ;call target
 (target)
+
+;randomly select a value from the values list
+;https://www.rosettacode.org/wiki/Pick_random_element#Racket
+(define (pick-item l)
+  (if (null? l)
+      ;if passed the null list passback 0
+      0
+      (list-ref l (random (length l)))))
+
+;call pick-item
+(pick-item values)
