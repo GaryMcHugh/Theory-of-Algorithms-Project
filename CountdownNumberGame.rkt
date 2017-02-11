398851d4539dde2ffea8412c55f1880e693341de
GARY MC HUGH <G00308668@gmit.ie>
2017-02-11 19:04:24 +0000

    added second function to try pick multiple numbers from a list

--- CountdownNumberGame.rkt
+++ CountdownNumberGame.rkt
@@ -19,4 +19,12 @@
       (list-ref l (random (length l)))))
 
 ;call pick-item
-(pick-item values)
+;(pick-item values)
+
+;nned to remove element when selected and add it to a new list (containg the 6 numbers)
+(define select-random
+  (lambda (ls)
+    (let ((len (length ls)))         ;; find out how long the list is
+      (list-ref ls (random len)))))  ;; pick one from 0 to the end
+
+(select-random (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
