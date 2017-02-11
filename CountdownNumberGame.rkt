562bacc0f0f5ed84796c0e00859b40c6c5613f90
GARY MC HUGH <G00308668@gmit.ie>
2017-02-11 18:39:13 +0000

    created lists

---
+++ CountdownNumberGame.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+;a list of the possible small values
+(define small (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10))
+
+;a list of the possible large values
+(define large (list 25 50 75 100))
