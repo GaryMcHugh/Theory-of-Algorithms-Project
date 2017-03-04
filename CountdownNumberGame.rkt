778e28108a47117da06cf3b9b275f360711180c4
GARY MC HUGH <G00308668@gmit.ie>
2017-03-04 17:42:21 +0000

    figuring out calculation

--- CountdownNumberGame.rkt
+++ CountdownNumberGame.rkt
@@ -9,6 +9,9 @@
 ;list to hold the 6 randomly selected numbers
 (define selected(list))
 
+;list of operators that will be used
+(define operators '(* / + -))
+
 ;generate a target value
 ;https://www.reddit.com/r/Racket/comments/32g9v3/how_do_you_generate_random_numbers/
 (define (target) (displayln (random 101 1000)))
@@ -45,14 +48,72 @@
 ;-----------------------------------------
 ;        Working out calculations
 ;-----------------------------------------
-
 'working 'out 'calculations
 
-;(permutations (list 1 2 3 4))
+(define t 125)
+(define a 5)
+(define b  25)
 
-(combinations (list 1 2 3 4) 0)
+(define l (list    
+          (+ a b)
+          (+ b a)
+          (- a b)
+          (- b a)
+          (/ a b)
+          (/ b a)
+          (* a b)
+          (* b a)
+))
+l
+
+(define x 10)
+(define y 5)
+(define z 2)
+
+(define m (list    
+          (+(+ x y) z)
+          (-(+ x y) z)
+          (/(+ x y) z)
+          (*(+ x y) z)
+          (+(+ y x) z)
+          (-(+ y x) z)
+          (/(+ y x) z)
+          (*(+ y x) z)
+          (+(- x y) z)
+          (-(- x y) z)
+          (/(- x y) z)
+          (*(- x y) z)
+          (+(- y x) z)
+          (-(- y x) z)
+          (/(- y x) z)
+          (*(- y x) z)
+          (+(/ x y) z)
+          (-(/ x y) z)
+          (/(/ x y) z)
+          (*(/ x y) z)
+          (+(/ y x) z)
+          (-(/ y x) z)
+          (/(/ y x) z)
+          (*(/ y x) z)
+          (+(* x y) z)
+          (-(* x y) z)
+          (*(* x y) z)
+          (-(* x y) z)
+          (+(* y x) z)
+          (-(* y x) z)
+          (/(* y x) z)
+          (*(* y x) z)
+))
+
+m
+
+'seperate
+
+(permutations (list 1 2 3 4))
+
+(combinations (list 1 2 3 4) 2)
 
 (define (plus l)
   (+(car l)(cadr l)))
 
-(map plus (combinations (list 1 2 3 4 5) 2))
+(map plus (combinations (list 1 2 3 4) 2))
