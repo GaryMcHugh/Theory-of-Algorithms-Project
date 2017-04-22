7b81676a327feba9958b506811dc0e8bac21e691
GARY MC HUGH <G00308668@gmit.ie>
2017-04-22 18:09:48 +0100

    added stack from class

--- CountDownGame.rkt
+++ CountDownGame.rkt
@@ -21,3 +21,14 @@
      [('^ (list x y s ___)) (cons (expt y x) s)]
      [(x s) (error "calculate-RPN: Cannot calculate the expression:" 
                    (reverse (cons x s)))])))
+
+(define (valid-rpn? e [s 0])
+  (if(null? e)
+     (if (= s 1) #t #f)
+     (if(= (car e) 1)
+         ;if the first thing on e is 1
+         (valid-rpn? (cdr e)(+ 1 s))
+         (if(> s 1)
+           (valid-rpn? (cdr e) (- 1 s))
+           #f)
+         )))
