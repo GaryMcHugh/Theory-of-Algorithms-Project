124999d12d5534cb28ef2c0b681c4ff8ad7a4029
GARY MC HUGH <G00308668@gmit.ie>
2017-04-22 17:14:27 +0100

    starting new script

---
+++ CountDownGame.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define operators '(* / + -))
+
+;-----------------------------------------
+;        Reverse Polish Notation
+;-----------------------------------------
+
+;https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#Racket
+'rpn-solver
+
+(define (calculate-RPN expr)
+  (for/fold ([stack '()]) ([token expr])
+    (printf "~a\t -> ~a~N" token stack)
+    (match* (token stack)
+     [((? number? n) s) (cons n s)]
+     [('+ (list x y s ___)) (cons (+ x y) s)]
+     [('- (list x y s ___)) (cons (- y x) s)]
+     [('* (list x y s ___)) (cons (* x y) s)]
+     [('/ (list x y s ___)) (cons (/ y x) s)]
+     [('^ (list x y s ___)) (cons (expt y x) s)]
+     [(x s) (error "calculate-RPN: Cannot calculate the expression:" 
+                   (reverse (cons x s)))])))
